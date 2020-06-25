abc1adbcf9e18a1522da90832b77a230b81191d9
vuquanghuy2001 <59812614+vuquanghuy2001@users.noreply.github.com>
2020-06-25 08:50:08 +0700

    update homework
    nộp sesion 2345

---
+++ Session_4/CodeSnippet_6.php
@@ -0,0 +1,10 @@
+<?php
+$A=$_GET['Asia'];
+$b=$_GET['AUtralia'];
+$C=$_GET['Europe'];
+$Name=$_GET['Continent'];
+echo "<BR>";
+echo "Continents:<BR><BR> Asia <BR><BR> Autralia <BR> Euro <BR><BR>";
+echo "The continent you have selectec is: $Name";
+?>
+
